9cc8ab7960557aebe4e61aff4650da3f77e961e8
ivrytb <ytb046@gmail.com>
2026-02-25 16:22:50 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,30 +1,12 @@
-from flask import Flask, request, Response, render_template_string, jsonify
-import requests
+from flask import Flask, jsonify, request, render_template_string
 import os
 
 app = Flask(__name__)
 YM_TOKEN = os.environ.get('YM_TOKEN')
 
-@app.route('/api/proxy_audio')
-def proxy_audio():
-    target_url = request.args.get('url')
-    # ה-Headers שקול מבשר דורשים
-    headers = {
-        'User-Agent': 'Mozilla/5.0',
-        'Referer': 'https://www.yiddish24.com/'
-    }
-    
-    def generate():
-        # אנחנו מושכים מקול מבשר ומזרימים לימות המשיח
-        r = requests.get(target_url, headers=headers, stream=True)
-        for chunk in r.iter_content(chunk_size=1024*1024):
-            yield chunk
-
-    return Response(generate(), content_type='audio/mpeg')
-
 @app.route('/api/get_config')
 def get_config():
-    return jsonify({"token": YM_TOKEN, "host": request.host})
+    return jsonify({"token": YM_TOKEN})
 
 @app.route('/')
 def home():
